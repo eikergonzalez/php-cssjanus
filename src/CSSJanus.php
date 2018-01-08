7866b8a6f7ad8ba8c7f4eb9f87b084452a41d8e5
Pablo Borowicz <eternoendless@gmail.com>
2018-01-08 10:57:29 +0100

    Fix indentation

--- src/CSSJanus.php
+++ src/CSSJanus.php
@@ -259,20 +259,24 @@ class CSSJanus {
 	 */
 	private static function fixLeftAndRight($css) {
 		if (extension_loaded('mbstring')) {
-      			mb_internal_encoding("UTF-8");
-      			mb_regex_encoding("UTF-8");
-      			$css = mb_eregi_replace(
-				    trim(rtrim(self::$patterns['left'], 'i'), '/'),
-				    self::$patterns['tmpToken'],
-				    $css
-				);
-      			$css = mb_eregi_replace(trim(rtrim(self::$patterns['right'], 'i'), '/'), 'left', $css);
-	    } else {
-      			$css = preg_replace(self::$patterns['left'], self::$patterns['tmpToken'], $css);
-      			$css = preg_replace(self::$patterns['right'], 'left', $css);
-	    }
-    		$css = str_replace(self::$patterns['tmpToken'], 'right', $css);
-    		return $css;
+			mb_internal_encoding("UTF-8");
+			mb_regex_encoding("UTF-8");
+			$css = mb_eregi_replace(
+				trim(rtrim(self::$patterns['left'], 'i'), '/'),
+				self::$patterns['tmpToken'],
+				$css
+			);
+			$css = mb_eregi_replace(
+				trim(rtrim(self::$patterns['right'], 'i'), '/'),
+				'left',
+				$css
+			);
+		} else {
+			$css = preg_replace(self::$patterns['left'], self::$patterns['tmpToken'], $css);
+			$css = preg_replace(self::$patterns['right'], 'left', $css);
+		}
+		$css = str_replace(self::$patterns['tmpToken'], 'right', $css);
+		return $css;
 	}
 
 	/**
